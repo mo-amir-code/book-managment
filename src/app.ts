f4a55e1a80266bd3d2b0676e9c16d8a0aafa6a1f
Mo Amir <mo.amir.code@gmail.com>
2024-04-27 21:41:12 +0530

    default get api added

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,12 @@ const PORT = 8080
 const app = express();
 
 app.use(express.json())
+app.use("/", (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Server is running smoothly"
+    }) 
+})
 app.use("/api/v1", routers)
 app.use(errorHandler)
 
