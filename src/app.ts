a5ab1473571ccd400af7beb6a32dbd26b9b0858a
Mo Amir <mo.amir.code@gmail.com>
2024-04-27 21:39:33 +0530

    Uninstalled moran

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,12 @@ import express from "express"
 import routers from "./routers"
 import { connectToDB } from "./utils/connectToDB";
 import { errorHandler } from "./middlewares/errors";
-import morgan from "morgan"
 
 const PORT = 8080
 
 const app = express();
 
 app.use(express.json())
-app.use(morgan("dev"))
 app.use("/api/v1", routers)
 app.use(errorHandler)
 
